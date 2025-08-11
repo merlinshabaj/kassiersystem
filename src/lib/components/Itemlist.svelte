947e637e268b478afbbcc484ad0ae987f66b912e
merlinshabaj <merlin.goessling@gmail.com>
2025-08-11 12:45:39 +0200

    Update

--- src/lib/components/Itemlist.svelte
+++ src/lib/components/Itemlist.svelte
@@ -1,14 +1,30 @@
 <script lang='ts'>
   import { type Item } from '$lib/types';
+  import { assert } from '$lib/utilities';
 
   let {items}: {items: Item[]} = $props();
   const total_price = $derived(
     items.reduce((sum, item) => sum + item.price * 100, 0)
   );
+
+  // TODO: find a way to hide the scrollbar when scrolling using js in the effect below
+  // otherwise it should be visible
+  let hide_scrollbar = $state(false);
+
+  let previous_items_length = items.length;
+
+  $effect(() => {
+    const list = document.getElementById('list');
+    if (previous_items_length < items.length) {
+      assert(list !== null);
+      list.scrollTop = list.scrollHeight;
+      previous_items_length = items.length;
+    }
+  });
 </script>
 
 <div class="flex flex-col m-2">
-  <div class="flex flex-col h-[50vh] overflow-scroll">
+  <div id="list" class="flex flex-col h-[50vh] overflow-scroll {hide_scrollbar ? 'scrollbar-hide' : ''}">
     {#each items as item}
       <div class="grid grid-cols-[9fr_1fr] my-0.5 rounded-sm bg-neutral-200 p-1 hover:bg-neutral-300 active:bg-neutral-400">
         <span>{item.name}</span>
